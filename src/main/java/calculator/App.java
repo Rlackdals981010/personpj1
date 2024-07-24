f01d82add5acf3f3f31e8fa1130c6a0263e6c76f
Robbie <robbie331@>
2024-07-24 10:46:03 +0900

    과제 1 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -2,7 +2,9 @@ package calculator;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
 
+        Calculator1 c1 = new Calculator1();
+        c1.play();
     }
 }
