4d0f10527322dba15fd6267e78c920e941847827
Robbie <robbie331@>
2024-07-24 13:09:35 +0900

    L2 요구사항 2 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import com.sun.source.tree.BreakTree;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -24,9 +26,12 @@ public class App {
             System.out.print("연산기호 입력하세요: ");
             char sign = br.readLine().charAt(0);        //2. 기호 입력
 
-//          double ret = 0; // 나눗셈 연산을 위해서 double 형으로 선언한다.
-            double ret = L2c.calculate(a, b, sign); //L2.
-
+          double ret = 0; // 나눗셈 연산을 위해서 double 형으로 선언한다.
+            try{
+                ret = L2c.calculate(a, b, sign);//L2.
+            } catch (MyException e){
+                System.out.println(e.getMessage());
+            }
 //            switch (sign){
 //                case '+':
 //                    ret = a+b;
@@ -55,25 +60,25 @@ public class App {
 ////            else
 ////                arr[count++]=ret;
 //
-//            arr.add(ret);   //collection에 맞는 저장 메소드 사용
-
+//          arr.add(ret);   //collection에 맞는 저장 메소드 사용
+            System.out.println(ret);
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)"); //7. remove 추가
             dis = br.readLine();
             if(dis.equals("remove")) {          //7. remove 입력시
-                arr.remove(0);           //7. 가장 오래된. 즉 idx=0 삭제
+                L2c.arr.remove(0);           //7. 가장 오래된. 즉 idx=0 삭제
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)"); //8. inquiry추가
             dis = br.readLine();
             if(dis.equals("inquiry")) {          //8. inquiry 입력시
-                for(double val : arr){
+                for(double val : L2c.arr){
                     System.out.print(val+" ");  //8. 내용 출력
                 }
                 System.out.println();
             }
 
 //          System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) (현 저장개수 "+count+"개)");
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) (현 저장개수 "+arr.size()+"개)");//4. exit 기능추가
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) (현 저장개수 "+L2c.arr.size()+"개)");//4. exit 기능추가
             dis = br.readLine();
             if(dis.equals("exit"))break;            //결정 객체에 exit 입력시 종료
         }
